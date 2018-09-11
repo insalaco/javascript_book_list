de6d91dd010e70396ec4252db8f22774ba1614f2
Michael Insalaco <insalaco01@gmail.com>
2018-09-11 16:29:33 -0400

    add local storage

--- appes6.js
+++ appes6.js
@@ -60,6 +60,52 @@ class UI {
   }
 }
 
+// local storage
+
+class Store {
+
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));   
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+      const ui = new UI;
+      // add book to ui
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   // get form values
@@ -80,6 +126,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   } else {
     // add book to list
     ui.addBookToList(book);
+ 
+    // add to local storage
+    Store.addBook(book);
 
     //show success
     ui.showAlert('Book Added!', 'success');
@@ -102,6 +151,9 @@ document.getElementById('book-list').addEventListener('click', function(e) {
 
   // delete book
   ui.deleteBook(e.target);
+
+  // Remove from local storage
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // show alert
   ui.showAlert('Book removed', 'success');
